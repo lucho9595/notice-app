f6b2169d1fc1c3bbd3eed86b74a1f0952251806a
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2024-02-03 12:48:55 -0300

    third commit

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+
+  categories: any[] = [
+    { values: 'general', name: 'General' },
+    { values: 'business', name: 'Business' },
+    { values: 'sports', name: 'Sports' },
+    { values: 'politics', name: 'Politics' },
+  ];
+
+  countries: any[] = [
+    { values: 'ar', name: 'Argentina' },
+    { values: 'br', name: 'Brazil' },
+    { values: 'eu', name: 'United States' },
+    { values: 'es', name: 'Spain' },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
